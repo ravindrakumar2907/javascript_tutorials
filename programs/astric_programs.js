c6a359c8a15dbd73beff2c0650a1d94986e3c2d0
ravindrakumar2907 <ravindramore6@gmail.com>
2022-11-24 20:06:57 +0530

    added new program files

--- programs/astric_programs.js
+++ programs/astric_programs.js
@@ -19,3 +19,75 @@ program to print the *
 8   8       8   8
 
 */
+
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+console.log("1 \n");
+let data = "";
+for(let i = 5; i > 0; i--){ // outer row
+    for(let j = 5; j > 0; j--){ // inner columns
+        data += " * ";
+    }
+    console.log(data);
+    data = "";
+    console.log("\n");
+}
+
+
+/*
+*
+**
+***
+****
+*/
+// row 0 -3, col 0-3
+console.log("2 \n");
+var str = "";
+for(let row = 0; row < 4; row++){ // row
+    for(let col = 0; col <= row; col++){ // col
+        str += " * ";
+    }
+    console.log(str);
+    console.log("\n");
+    str = "";
+}
+
+/*
+ *  *  *  *  *
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+*/
+console.log("3 \n");
+var str = "";
+for(let row = 0; row <= 4; row++){ // row 0 3
+    for(let col = 4; col >= row; col--){ // col col = 1, row = 3
+        str += " * ";
+    }
+    console.log(str);
+    console.log("\n");
+    str = "";
+}
+
+
+
+console.log(" 4\n");
+var str = "";
+for(let row = 0; row < 4; row++){ // row 0 3
+    for(let col = 4; col <= 4; col--){ // col col = 1, row = 3
+        str += " * ";
+    }
+    console.log(str);
+    console.log("\n");
+    str = "";
+}
+
+
