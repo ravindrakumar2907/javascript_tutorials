6e5c2a726f1c9d773307b9b4aa0732b1571ab25d
ravindrakumar2907 <ravindramore6@gmail.com>
2022-12-12 20:21:56 +0530

    added new features in demo

--- demo/js/script.js
+++ demo/js/script.js
@@ -31,8 +31,10 @@ function login(){
         isAdmin = registerObject.userType;
         if(username === email && password=== passwordDb && userType === isAdmin){
             //redirect to admin page
+            alert("Login success redirecting to admin section.");
             location.href = "admin.html";
         }else if(username === email && password=== passwordDb && userType === "user"){
+            alert("Login success redirecting to home page.");
             location.href = "index.html";
         }else{
             alert("Username or password is not valid");     
@@ -69,6 +71,68 @@ function registerFunction(){
     alert("Registration is success.");
 }
 
+function getAllUsersDetail(){
+    alert("Reading Users information from DB.");
+    
+    let tableObject = document.getElementById("tableBody");
+
+    let local = localStorage;
+    for (var key in local) {
+        if(localStorage.getItem(key) != null){
+            console.log(key);
+            let jsonData = localStorage.getItem(key)
+            registerObject = JSON.parse(jsonData);
+                //tableObject.insetRow();
+             // Create row element
+            let row = document.createElement("tr")
+      
+            // Create cells
+            let c1 = document.createElement("td") // node
+            let c2 = document.createElement("td")
+            let c3 = document.createElement("td")
+            let c4 = document.createElement("td")
+            c1.innerHTML = registerObject.firstName; // String
+            c2.innerHTML = registerObject.lastName;
+            c3.innerHTML = registerObject.email;
+            c4.innerHTML = registerObject.fee;
+
+            // Append cells to row
+            row.appendChild(c1);
+            row.appendChild(c2);
+            row.appendChild(c3);
+            row.appendChild(c4);
+            
+            // Append row to table body
+            tableObject.appendChild(row)
+        }
+    }
+}
+
+function getAllUsersDetail1(){
+    alert("Reading Users information from DB.");
+    
+    let tableObject = document.getElementById("tableBody");
+
+    let local = localStorage;
+    for (var key in local) {
+        if(localStorage.getItem(key) != null){
+            console.log(key);
+            let jsonData = localStorage.getItem(key)
+            registerObject = JSON.parse(jsonData);
+                //tableObject.insetRow();
+            let row = tableObject.insertRow(-1);
+            // Create table cells
+            let c1 = row.insertCell(0);
+            let c2 = row.insertCell(1);
+            let c3 = row.insertCell(2);
+            let c4 = row.insertCell(3);
+            c1 = registerObject.firstName;
+            c2 = registerObject.lastName;
+            c3 = registerObject.email;
+            c4 = registerObject.fee;
+        }
+    }
+}
 
 // login button event
 
@@ -78,4 +142,10 @@ if(document.getElementById("login")){
 if(document.getElementById("registerUser")){
     document.getElementById("registerUser").addEventListener("click", registerFunction);
 }
+
+/*
+if(document.getElementById("getAllUsers")){
+    document.getElementById("getAllUsers").addEventListener("click", getAllUsersDetail);
+}*/
+
 
