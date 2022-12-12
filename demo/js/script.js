a35065fe9477ef9c8084db2e891028b02cfbf5f6
ravindrakumar2907 <ravindramore6@gmail.com>
2022-12-12 19:16:51 +0530

    adding new changes to demo app

--- demo/js/script.js
+++ demo/js/script.js
@@ -11,21 +11,71 @@
 
 function login(){
     //username password userType
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let userType = document.getElementById("userType").value;
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
+    let userType = document.getElementById("userType").value.trim();
+    let email = null;
+    let passwordDb = null;
+    let isAdmin = false;
 
     if(isEmpty(username) || isEmpty(password) || isEmpty(userType)){
         alert("Please check your input.");
         return false;
     }
     
-    if(username === "ravi" && password=== "ravi" && userType === "admin"){
-        //redirect to admin page
-        location.href = "admin.html";
-    }else if(username === "ravi" && password=== "ravi" && userType === "user"){
-        location.href = "index.html";
+    let registerObject = localStorage.getItem(username);
+    if(registerObject != null &&  registerObject != undefined){
+        registerObject = JSON.parse(registerObject);
+        email = registerObject.email;
+        passwordDb = registerObject.password;
+        isAdmin = registerObject.userType;
+        if(username === email && password=== passwordDb && userType === isAdmin){
+            //redirect to admin page
+            location.href = "admin.html";
+        }else if(username === email && password=== passwordDb && userType === "user"){
+            location.href = "index.html";
+        }else{
+            alert("Username or password is not valid");     
+        }
     }
+
 }
+
+function registerFunction(){
+    let firstName = document.getElementById("firstName").value.trim();
+    let lastName = document.getElementById("lastName").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let subject = document.getElementById("subject").value.trim();
+    let fee = document.getElementById("fee").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if(isEmpty(firstName) && isEmpty(firstName) && isEmpty(firstName)
+     && isEmpty(firstName) && isEmpty(firstName) && isEmpty(password)){
+        alert("Please check your input.");
+        return false;
+    }
+
+    registerObj = {
+        firstName : firstName,
+        lastName : lastName,
+        email : email,
+        password: password,
+        subject : subject,
+        fee : fee
+    }
+
+    
+    localStorage.setItem(email, JSON.stringify(registerObj));
+    alert("Registration is success.");
+}
+
+
 // login button event
-document.getElementById("login").addEventListener("click", login);
+
+if(document.getElementById("login")){
+    document.getElementById("login").addEventListener("click", login);
+}
+if(document.getElementById("registerUser")){
+    document.getElementById("registerUser").addEventListener("click", registerFunction);
+}
+
